b5fa495cbe3edb7fadbfdf129a026c73ff33e05d
GowthamSithagai <131848554+GowthamSithagai@users.noreply.github.com>
2023-04-25 23:17:41 -0700

    Initial commit

---
+++ DemoAutomation.java
@@ -0,0 +1,21 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.google.co.in");
+		WebElement click = driver.findElement(By.name("q"));
+		click.sendKeys("Agni"+Keys.ENTER);
+
+	}
+
+}
